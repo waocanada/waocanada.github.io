b43bb11171539f1b9300d3483d4a0928700c5834
Nicole Zhao <yy3zhao@uwaterloo.ca>
2017-03-11 22:50:01 -0500

    Final layout + most content

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -4,11 +4,10 @@
         var rhill = {lat: 43.8815363, lng: -79.4387149};
         var community = {lat: 43.6593425, lng: -79.36708};
         var aplus = {lat: 43.5375324, lng: -79.8745092};
-        // var  = {lat: , lng: };
-        // var  = {lat: , lng: };
-        // var  = {lat: , lng: };
-        // var  = {lat: , lng: };
-        // var  = {lat: , lng: };
+        var access = {lat: 43.6714965, lng: -79.4933432};
+        var acton = {lat: 43.6331924, lng: -80.0350459};
+        var adam = {lat: 43.6577554, lng: -79.4330363};
+        var blake  = {lat: 43.6730145, lng: -79.338275};
         // var  = {lat: , lng: };
         // var  = {lat: , lng: };
         // var  = {lat: , lng: };
@@ -20,7 +19,7 @@
 
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 9,
-          center: twospirited, rhill, community, aplus
+          center: twospirited, rhill, community, aplus, access, acton, adam, blake
         });
         
 
@@ -38,6 +37,22 @@
         });
         var marker = new google.maps.Marker({
           position: aplus,
+          map: map
+        });
+        var marker = new google.maps.Marker({
+          position: access,
+          map: map
+        });
+        var marker = new google.maps.Marker({
+          position: acton,
+          map: map
+        });
+        var marker = new google.maps.Marker({
+          position: adam,
+          map: map
+        });
+        var marker = new google.maps.Marker({
+          position: blake,
           map: map
         });
       }
